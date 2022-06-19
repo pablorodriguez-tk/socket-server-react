fff52d118b433b06bbe0c4656a6f5bce023d04d8
Pablo Rodriguez <pablor@bombieri.com.ar>
2022-06-19 20:28:02 -0300

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,43 @@
+//Servidor de Express
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    //Http server
+    this.server = http.createServer(this.app);
+    //Configuracion del socket server
+    this.io = socketio(this.server, {
+      // configuraciones
+    });
+  }
+
+  middlewares() {
+    //Desplegar el directorio publico
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+
+  configurarSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    //Inicializar Middlewares
+    this.middlewares();
+
+    //Inicializar Sockets
+    this.configurarSockets();
+
+    //Inicializar Server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en puerto: ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
