9c81e58cd3e3ae2d8c3258d82fc868f7f5334290
Pablo Rodriguez <pablorodriguez88@gmail.com>
2022-06-19 21:42:08 -0300

    feat: version lista para produccion

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
+const cors = require("cors");
 
 class Server {
   constructor() {
@@ -13,13 +14,18 @@ class Server {
     this.server = http.createServer(this.app);
     //Configuracion del socket server
     this.io = socketio(this.server, {
-      // configuraciones
+      cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+      },
     });
   }
 
   middlewares() {
     //Desplegar el directorio publico
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+    //Cors
+    this.app.use(cors());
   }
 
   configurarSockets() {
